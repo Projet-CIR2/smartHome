f56f471765f4135c06c2a4d81a01af1987b242ff
Arthur Pineau <58333043+Arthur-P0@users.noreply.github.com>
2021-04-09 16:18:55 +0200

    case

---
+++ js/scriptmagique.js
@@ -0,0 +1,2 @@
+//document.getElementsByClassName('container-fluid').getElementsByClassName('row').getElementsByClassName('col-sm-4').getElementsByClassName('row').getElementsByClassName('col-sm-6').getElementsByClassName('btn-group').getElementById('MaJ');
+document.getElementById('MaJ');
