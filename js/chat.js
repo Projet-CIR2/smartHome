0e5b769d4b8efba49c17c35c203cb8d2841338e4
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-04-16 11:28:52 +0200

    Fonctionnement chat

--- js/chat.js
+++ js/chat.js
@@ -1,18 +1,27 @@
+
+
 let chat = (function () {
+    function afficheTexte(texte) {
+        let divChat = document.getElementById('chat');
+
+        if (divChat.childElementCount >= 15) divChat.removeChild(divChat.lastElementChild);
+
+        let p = document.createElement('p');
+        divChat.prepend(p);
+        p.textContent = texte;
+    }
+
     return {
-        afficheTexte(texte) {
-            let divChat = document.getElementById('chat');
-
-            if (divChat.childElementCount >= 10) divChat.removeChild(divChat.lastElementChild);
-
-            let p = document.createElement('p');
-            divChat.prepend(p);
-            p.textContent = window.Date();
+        texteEvent() {
+            if (Math.floor(Math.random() * 2)) {
+                console.log(Math.floor(Math.random() * json.events.length));
+                console.log(json.events[Math.floor(Math.random() * json.events.length)])
+                afficheTexte(json.events[Math.floor(Math.random() * json.events.length)])
+            }
         },
 
         lanceTexte() {
-            let compteur = 0;
-            setInterval(this.afficheTexte, 1000, 'coucou');
+            setInterval(this.texteEvent, 1000);
         }
     }
 })();
---
+++ json/events.js
@@ -0,0 +1,6 @@
+let json = {
+    events: [
+        "Le coco est de retour",
+        "Arthur est allé chez le dentiste"
+    ]
+}
