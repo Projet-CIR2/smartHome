5f503c39abe373fb99b5137071ef42db55b98153
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-05-01 15:34:13 +0200

    Ajout de la difficulté

--- js/models/gameView.js
+++ js/models/gameView.js
@@ -3,6 +3,7 @@ class GameView {
         this.humeur = 100; // en %
         this.argent = 10; // en €
         this.debit = 100; // en %
+        this.difficulte = 0; // 1, 2, ou 3
 
         this.barreHumeur = new Barres('humeur');
         this.barreArgent = new Barres('argent');
