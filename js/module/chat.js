a8a16219c6320dc2ff5dafa8328744a0b5e6ff3a
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-05-01 15:15:56 +0200

    Ajout de commentaire + modif logique chat

--- js/module/chat.js
+++ js/module/chat.js
@@ -1,4 +1,5 @@
 let chat = (function () {
+    // ajoute le texte au chat
     function afficheTexte(texte, couleur) {
         let divChat = document.getElementById('chat');
 
@@ -10,32 +11,37 @@ let chat = (function () {
         if (couleur !== undefined) p.setAttribute('style', 'color:' + couleur);
     }
 
+    // affiche les textes n'ayant pas d'impact
+    function texteEvent() {
+        if (Math.floor(Math.random() * 2)) {
+            afficheTexte(json.events[Math.floor(Math.random() * json.events.length)]);
+        }
+    }
+
+    // affiche les textes ayant un impact sur le jeu
+    function texteImportant() {
+        let event;
+        // une chance sur 2 que l'événement soit positif ou négatif
+        if (Math.floor(Math.random() * 2)) {
+            event = json.evenements_important.positif[Math.floor(Math.random() * json.evenements_important.positif.length)];
+            afficheTexte(event.description, 'blue');
+        }
+        else {
+            event = json.evenements_important.negatif[Math.floor(Math.random() * json.evenements_important.negatif.length)];
+            afficheTexte(event.description, 'red');
+        }
+
+        // on modifie les valeurs du jeu
+        VarGame.modifVar('humeur', event.humeur);
+        VarGame.modifVar('argent', event.argent);
+        VarGame.modifVar('debit', event.debit);
+    }
+
     return {
-        texteEvent() {
-            if (Math.floor(Math.random() * 2)) {
-                afficheTexte(json.events[Math.floor(Math.random() * json.events.length)]);
-            }
-        },
-
-        texteImportant() {
-            let event;
-            if (Math.floor(Math.random() * 2)) {
-                event = json.evenements_important.positif[Math.floor(Math.random() * json.evenements_important.positif.length)];
-                afficheTexte(event.description, 'blue');
-            }
-            else {
-                event = json.evenements_important.negatif[Math.floor(Math.random() * json.evenements_important.negatif.length)];
-                afficheTexte(event.description, 'red');
-            }
-
-            VarGame.modifVar('humeur', event.humeur);
-            VarGame.modifVar('argent', event.argent);
-            VarGame.modifVar('debit', event.debit);
-        },
-
+        // ajout des textes
         lanceTexte() {
-            setInterval(this.texteEvent, 1000);
-            setInterval(this.texteImportant, 10000);
+            setInterval(texteEvent, 90000);
+            setInterval(texteImportant, 30000);
         }
     }
 })();
--- js/module/varGame.js
+++ js/module/varGame.js
@@ -5,7 +5,7 @@ let VarGame = (function () {
         initView(view_) {
             view = view_;
 
-            // ajout des valeurs toutes les 2 secondes
+            // ajout des valeurs toutes les 30 secondes
             setInterval(() => {
                 this.modifVar('humeur', -2);
             }, 30000);
@@ -17,6 +17,7 @@ let VarGame = (function () {
             }, 300000);
         },
 
+        // modifie la variable de jeu du type de nb
         modifVar(type, nb) {
             if (type === 'humeur' && view + nb >= 0 && view + nb <= 100) view.humeur += nb;
             if (type === 'argent') view.argent += nb;
