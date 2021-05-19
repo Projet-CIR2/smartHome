673d5fa91008b5db95c5aa1c7681bc0a83a6fc45
Pierre-Louis FOURNET <pierre-louis.fournet@student.yncrea.fr>
2021-05-19 13:53:03 +0200

    Affichage

--- js/formulaire.js
+++ js/formulaire.js
@@ -22,6 +22,28 @@ let createObjetMaj = function (mOru, title, text, btn) {
         button.onclick = () => {
             div.remove();
             achat.addMaj(title);
+            let afficheAchat = document.getElementById('achat');
+            let img = document.createElement('img');
+            let pAchat = document.createElement('p');
+
+            afficheAchat.setAttribute('style', 'position: absolute; margin-top: 20%; margin-left: 35%; background-color: #BFB99E; border-radius: 4px; height: 30%; width: 30%;');
+            img.src = "./img/cam.png";
+            img.setAttribute('style', 'height: 120; width: 120px; margin-left: 25%; margin-top: 13px;');
+            img.id = 'image';
+
+            pAchat.textContent = "Vous avez acheté " + title;
+            pAchat.setAttribute('style', 'color: white; margin-top: 10px; text-align:center;');
+            pAchat.id = 'achatTxt'
+
+            let pAchatHTML = document.getElementById('achatTxt');
+            let imgHTML = document.getElementById('image');
+            
+            if(afficheAchat.childElementCount == 2) {
+                afficheAchat.removeChild(imgHTML);
+                afficheAchat.removeChild(pAchatHTML);
+            }
+            afficheAchat.appendChild(img);
+            afficheAchat.appendChild(pAchat);
         };    
     }
 }
