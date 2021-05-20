69edfbd92a12dc626252c66f353d3f3757ec35a5
Pierre-Louis FOURNET <pierre-louis.fournet@student.yncrea.fr>
2021-05-20 14:27:26 +0200

    Info chat

--- js/scenarioIndex.js
+++ js/scenarioIndex.js
@@ -1,3 +1,21 @@
 socket.on('scena', (elmt) => {
     console.log(elmt);
+    console.log(elmt.titre);
+
+    let chat = document.getElementById('chat');
+    chat.setAttribute('style', 'font-size: 12px;');
+
+    let p = document.createElement('p');
+    p.textContent = "Scénario activé : " + elmt.titre;
+    p.setAttribute('style', 'text-shadow: inherit');
+
+    let desc = document.createElement('p');
+    desc.textContent = "Description : " + elmt.description_debut;
+    desc.setAttribute('style', 'text-shadow: inherit');
+
+    chat.appendChild(desc);
+    chat.appendChild(p);
+    
 });
+
+
