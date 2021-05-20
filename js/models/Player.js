200b18a466a205fb3b1ee98f2d3d3fff47c1b569
Califano9577 <amaury.giot@student.yncrea.fr>
2021-05-20 16:40:03 +0200

    class player

--- js/models/Player.js
+++ js/models/Player.js
@@ -1,30 +1,54 @@
-class Player  {
-    constructor(scene_, x, y){
-        
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y){
 
-        let scene = scene_;
-        //const idPlayer = "perso1"; //+ id.toString();
+
         let pos = convert([x,y]);
-        //super(scene, pos[0], pos[1], 'perso1');
-        //scene.add.existing(this);
-        let hab = scene.physics.add.sprite(pos[0], pos[1], 'perso1');
+        super(scene, pos[0], pos[1], 'perso1');
 
-       
-    
 
+        scene.add.existing(this);
+
+        this.initAnim();
     }
 
-    update(){
 
-        movePlayer2(hab, 2,11);
+
+    initAnim() {
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('perso1', { start: 0, end: 0 }),
+            frameRate: 10,
+            repeat: -1
+    
+        })
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('perso1', { start: 1, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('perso1', { start: 2, end: 2 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('perso1', { start: 3, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'face',
+            frames: this.anims.generateFrameNumbers('perso1', { start: 3, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
         
+
     }
-
-    
-
-
-
-    
 }
 
 
@@ -42,64 +66,64 @@ function movePlayer2(player, x, y) {
     let marge = 2;
 
     if (playerXRound < x-marge || playerXRound > x+marge || playerYRound < y-marge || playerYRound > y +marge) {
-        
-        if (playerXRound > x && playerYRound >y) {
-            player.body.velocity.x -= speedX;
-            player.body.velocity.y -= speedY;
-            player.anims.play('left');
-            lastDirection = 0;
 
-        }
-        if (playerXRound < x && playerYRound < y) {
-            player.body.velocity.x += speedX;
-            player.body.velocity.y += speedY
-            player.anims.play('right');
-            lastDirection = 1;
+    if (playerXRound > x && playerYRound >y) {
+    player.body.velocity.x -= speedX;
+    player.body.velocity.y -= speedY;
+    player.anims.play('left');
+    lastDirection = 0;
+
+}
+    if (playerXRound < x && playerYRound < y) {
+    player.body.velocity.x += speedX;
+    player.body.velocity.y += speedY
+    player.anims.play('right');
+    lastDirection = 1;
 
 
-        }
-        if (playerYRound > y && playerXRound < x) {
-            player.body.velocity.y -= speedY;
-            player.body.velocity.x += speedX;
+}
+    if (playerYRound > y && playerXRound < x) {
+    player.body.velocity.y -= speedY;
+    player.body.velocity.x += speedX;
 
-            player.anims.play('up');
-            lastDirection = 2;
+    player.anims.play('up');
+    lastDirection = 2;
 
-        }
-        if (playerYRound < y && playerXRound > x) {
-            player.body.velocity.y += speedY;
-            player.body.velocity.x -= speedX;
+}
+    if (playerYRound < y && playerXRound > x) {
+    player.body.velocity.y += speedY;
+    player.body.velocity.x -= speedX;
 
-            player.anims.play('down');
-            lastDirection = 3;
+    player.anims.play('down');
+    lastDirection = 3;
 
-        }
-    } 
+}
+}
 
     else {
-        player.anims.stop();
-        player.x = x;
-        player.y = y;
-        switch(lastDirection){
-            case 0:
-                player.anims.play('left');
-                break;
-            case 1:
-                player.anims.play('right');
-                break;
-            case 2 :
-                player.anims.play('up');
-                break;
-            case 3:
-                player.anims.play('down');
-                break;
-            default:
-                player.anims.play('down');
-                break;
-                
-        }
-        return 1;
-    }
+    player.anims.stop();
+    player.x = x;
+    player.y = y;
+    switch(lastDirection){
+    case 0:
+    player.anims.play('left');
+    break;
+    case 1:
+    player.anims.play('right');
+    break;
+    case 2 :
+    player.anims.play('up');
+    break;
+    case 3:
+    player.anims.play('down');
+    break;
+    default:
+    player.anims.play('down');
+    break;
+
+}
+    return 1;
+}
 }
 
 
@@ -113,9 +137,9 @@ function cheminPath() {
         let coords = chemin[destinationInter];
         x = coords[1];
         y = coords[0];
-        
-       
-       let pos = convert([x,y]);
+
+
+        let pos = convert([x,y]);
         if (movePlayer2(player,pos[0],pos[1])) {
             if (destinationInter < cheminSize) destinationInter++;
         }
@@ -127,7 +151,7 @@ function cheminPath() {
 
 function convert([x, y]) {
     let posX = 135 +x*128 - y * 128;
-    let posY = 280 + y* 64 + x*64; 
+    let posY = 280 + y* 64 + x*64;
 
     return [posX, posY];
 }
